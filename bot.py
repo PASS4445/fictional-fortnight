0a2d03810337353ca1a5d50894f7a5b24963b4ee
PASS4445 <89130327+PASS4445@users.noreply.github.com>
2021-09-12 14:17:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
-for submission in submissions:
+import praw
+
+reddit = praw.Reddit('Cummy')
+
+subreddit = reddit.subreddit("botte1t")
+
+for submission in subreddit.hot(limit=8):
                 if submission['data']['is_self'] and submission['data']['id'] not in submitted:
                     sleep(self.options.submit_rate)
                     self.submit(submission['data'], modhash)
